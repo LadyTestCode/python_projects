ee43171e9099847ae51ecee5e505f6847a27b144
LadyTestCode <145100974+LadyTestCode@users.noreply.github.com>
2023-10-16 18:45:32 +0300

    Add files via upload

--- test_pet_api.py
+++ test_pet_api.py
@@ -4,177 +4,246 @@ import json
 import resources.urls as urls
 import Steps.support_steps as support_steps
 import Steps.create_request_json as create_request_json
+import Steps.request_steps as requests_steps
+import Steps.assert_steps as assert_steps
 
 #************** Тестирование API для управления питомцами **************
 
 #***** Запросы POST *****
 
+# проверка создания питомца
 @pytest.mark.smoke_regression
-def test_post_pet():
-    #создаем данные для запроса
-    request = create_request_json.generate_json_pet("cats")
-    response_post = requests.post(urls.url_pet_post, json=request)
+@pytest.mark.full_regression
+@pytest.mark.parametrize('type',
+                         [
+                             create_request_json.generate_json_pet_required_param(),
+                             create_request_json.generate_json_pet()
+                         ],
+                         ids=["required_param", "all_param"]
+                         )
+def test_post_pet(type):
+    # создаем данные json для запроса
+    request = type
+    print(request)
+    # отправляем запрос
+    response_post = requests_steps.request_post(urls.url_pet_post, request)
     print("result=", response_post.json())
-    #проверяем, что вернулся непустой ID созданного питомца
-    assert response_post.json()['id'] is not None
-    response_get = requests.get(urls.url_pet_get_id(str(response_post.json()['id'])))
+    # проверяем, что вернулся непустой ID созданного питомца
+    assert_steps.assert_not_none_id(response_post)
+    # проверяем, что созданного питомца можно получить запросом get
+    response_get = requests_steps.request_get(urls.url_pet_get_id(str(response_post.json()['id'])))
     print("result get =", response_get.json())
-    assert response_get.json () ['id'] == response_post.json() ['id']
+    # проверяем, что вернулся именно тот ID  питомца, с которым его создавали
+    assert_steps.assert_equals_response_ids(response_get, response_post)
 
+# проверка создания питомца с невалидными даннми
 @pytest.mark.full_regression
 def test_post_name_negative():
-    #создаем json c невалидным значением поля category.name
+    # создаем json c невалидным значением поля category.name
     request = create_request_json.generate_json_pet_incorrect()
-    response_post = requests.post(urls.url_pet_post, json=request)
+    # отправляем запрос
+    response_post = requests_steps.request_post(urls.url_pet_post, request)
     print("result =", response_post.json())
-    assert response_post.json()['message'] == "something bad happened"
+    # проверяем ответ
+    assert_steps.assert_equals_response_values(response_post.json()['message'],"something bad happened")
 
+# проверка возможности загрузить файл для питомца
 @pytest.mark.smoke_regression
+@pytest.mark.full_regression
 def test_post_pet_uploadImage():
-    #проверка загрузки файла для питомца
-    request = create_request_json.generate_json_pet("cats")
-    response_post = requests.post(urls.url_pet_post, json=request)
+    # создаем json для запроса
+    request = create_request_json.generate_json_pet_required_param()
+    # отправляем запрос и создаем питомца
+    response_post = requests_steps.request_post(urls.url_pet_post, request)
     print("result=", response_post.json())
-    assert response_post.json()['id'] is not None
-
+    assert_steps.assert_not_none_id (response_post)
+    # работа с файлом (позже вынести файл куда - нибудь в resources)
     fp = open('/Users/20071554/Desktop/test.txt', 'rb')
     files = {'file': fp}
-    response_post_image = requests.post(urls.url_pet_post_uploadimage(str(response_post.json()['id'])), files=files)
+    # отправляем запрос на загрузку файла
+    response_post_image = requests_steps.request_post_image(urls.url_pet_post_uploadimage(str(response_post.json()['id'])), files)
     print(response_post_image)
-    assert response_post_image.status_code == 200
+    # проверяем, что ответ содержит статус 200
+    assert_steps.assert_response_has_status (response_post_image, 200)
+    # закрываем чтение файла
     fp.close()
 
+# проверка загрузки файла для несуществующего питомца
 @pytest.mark.full_regression
 def test_post_pet_negative_uploadImage():
-    # проверка загрузки файла для несуществующего питомца
+    # работа с файлом (позже вынести файл куда - нибудь в resources)
     fp = open('/Users/20071554/Desktop/test.txt', 'rb')
     files = {'file': fp}
-    response_post_image = requests.post(urls.url_pet_post_uploadimage(support_steps.generate_random_letter_strings(6)), files=files)
-    assert response_post_image.status_code == 404
+    # пробуем загрузить файл для несуществующего питомца
+    response_post_image = requests_steps.request_post_image(urls.url_pet_post_uploadimage(support_steps.generate_random_letter_strings(6)), files)
+    # проверяем, что ответ содержит статус 404
+    assert_steps.assert_response_has_status (response_post_image, 404)
+    # закрываем чтение файла
     fp.close()
 
-#изменение данных о питомце запросом POST
+#проверяем изменение данных о питомце запросом POST
 @pytest.mark.smoke_regression
+@pytest.mark.full_regression
 def test_post_pet_id():
-    request = create_request_json.generate_json_pet("cats")
-    response_post = requests.post(urls.url_pet_post, json=request)
+    # создаем json с данными о питомце
+    request = create_request_json.generate_json_pet_required_param()
+    # отправляем запрос на создание питомца
+    response_post = requests_steps.request_post(urls.url_pet_post, request)
     print("result=", response_post.json())
-    assert response_post.json()['id'] is not None
-    #изменяем кличку питомца
+    # убеждаемся, что питомец создан, вернулся непустой ID
+    assert_steps.assert_not_none_id (response_post)
+    # изменяем кличку питомца
     newname = support_steps.generate_random_letter_strings(6)
     datamy="name=" + newname +"&status=sold"
-    response_post_update = requests.post(urls.url_pet_get_id(str(response_post.json()['id'])), data=datamy)
+    # отправляем запрос на изменение питомца
+    response_post_update = requests_steps.request_post_update(urls.url_pet_get_id(str(response_post.json()['id'])), datamy)
     print ("response_updated =" ,response_post_update.json() )
-    assert response_post_update.status_code ==200
-    response_get = requests.get(urls.url_pet_get_id(str(response_post.json()['id'])))
+    # проверяем, что ответ содержит статус 200
+    assert_steps.assert_response_has_status (response_post_update, 200)
+    # отправляем запрос на получение данных питомца
+    response_get = requests_steps.request_get(urls.url_pet_get_id(str(response_post.json()['id'])))
     print("result get =", response_get.json())
-    #проверяем, что кличка изменена
-    assert response_get.json()['name']==newname
+    # проверяем, что кличка действительно изменена
+    assert_steps.assert_equals_response_valuesassert(response_get.json()['name'], newname)
 
+#проверка, что нельзя измененить несуществующего питомца
 @pytest.mark.full_regression
-def test_post_nagative_pet_id():
-    #проверка изменения несуществующего питомца
+def test_post_negative_pet_id():
+    # задаем произвольное имя питомцв
     newname = support_steps.generate_random_letter_strings(6)
+    # готовим и выполняем запрос на обновление
     datamy = "name=" + newname +"&status=sold"
-    response_post_update = requests.post(urls.url_pet_get_id(support_steps.generate_random_number_strings(6)), data=datamy)
+    response_post_update = requests_steps.request_post_update(urls.url_pet_get_id(support_steps.generate_random_number_strings(6)), datamy)
     print("response_updated =", response_post_update.json())
-    assert response_post_update.status_code == 404
+    # проверяем, что ответ содержит статус 404
+    assert_steps.assert_response_has_status (response_post_update, 404)
+
 
 #***** Запросы PUT *****
+#проверка возможности изменить кличку питомца череp put
 @pytest.mark.smoke_regression
+@pytest.mark.full_regression
 def test_put_pet():
-    #проверка возможности изменить кличку питомца
-    #создаем питомца
-    request = create_request_json.generate_json_pet("cats")
-    response_post = requests.post(urls.url_pet_post, json=request)
+    # создаем питомца
+    request = create_request_json.generate_json_pet_required_param()
+    response_post = requests_steps.request_post(urls.url_pet_post, request)
     print("result=", response_post.json())
-    #формируем данные для обновления
+    # формируем данные для обновления
     request_put = create_request_json.generate_json_update_pet(str(response_post.json()["id"]))
-    response_put = requests.put(urls.url_pet_post, json=request_put)
+    # отправляем запрос на обновление
+    response_put = requests_steps.request_put(urls.url_pet_post, request_put)
     print("result_put=", response_put.json())
-    response_get = requests.get(urls.url_pet_get_id(str(response_post.json()['id'])))
-    #убеждаемся, что имя изменено
-    assert request_put["name"] == response_get.json()["name"]
-    assert response_put.json()["name"] == response_get.json()["name"]
+    # отправляем запрос на получение
+    response_get = requests_steps.request_get(urls.url_pet_get_id(str(response_post.json()['id'])))
+    # убеждаемся, что имя изменено
+    assert_steps.assert_equals_response_values(request_put["name"],response_get.json()["name"])
+    assert_steps.assert_equals_response_values(response_put.json()["name"],response_get.json()["name"])
 
+# Проверка, что нельзя обновить несуществующего питомца
 @pytest.mark.full_regression
 def test_negative_put_pet():
-    request_put = create_request_json.generate_json_update_pet(support_steps.generate_random_number_strings(6))
+    # формируем json
+    request_put = create_request_json.generate_json_update_pet(support_steps.generate_random_number_strings(9))
     print(request_put)
-    # обращаемся с запросом на обновление питомца, которого не создавали:
-    url_put_pet = urls.url_pet_post
-    response_put_pet = requests.put(url_put_pet, json=request_put)
+    # отправляем запросом на обновление питомца, которого не создавали:
+    response_put_pet = requests_steps.request_put(urls.url_pet_post, request_put)
     print("response_put_pet =", response_put_pet.status_code)
-    assert response_put_pet.status_code == 404
+    # проверяем, что ответ содержит статус 404
+    assert_steps.assert_response_has_status (response_put_pet, 404)
 
 #***** Запросы GET *****
 
-#получить питомца по ID
+# проверка получения питомца по ID
 @pytest.mark.smoke_regression
+@pytest.mark.full_regression
 def test_get():
-    #создаем питомца
-    request = create_request_json.generate_json_pet("cats")
-    response_post = requests.post(urls.url_pet_post, json=request)
-    #получаем питомца
-    response = requests.get(urls.url_pet_get_id(str(response_post.json()['id'])))
-    #смотрим, что в ответе
+    # создаем json для питомца
+    request = create_request_json.generate_json_pet()
+    # отправляем запрос на создание питомца
+    response_post = requests_steps.request_post(urls.url_pet_post, request)
+    # отправляем запрос на получение питомца
+    response = requests_steps.request_get(urls.url_pet_get_id(str(response_post.json()['id'])))
+    # смотрим, что в статус ответа 200 и что в ответе получили данные питомца
     print("result =", response.json())
-    assert response.json()['id'] == response_post.json()['id']
-    assert response.json()['status'] == 'sold'
-    assert response.status_code == 200
+    assert_steps.assert_equals_response_ids(response,response_post)
+    assert_steps.assert_equals_response_values(response.json()['status'], 'sold')
+    assert_steps.assert_response_has_status (response, 200)
 
+# проверка запроса на получение несуществующего питомца
 @pytest.mark.full_regression
 def test_get_pet_id_negative ():
-    #ищем несуществующего питомца
-    response_get = requests.get(urls.url_pet_get_id(support_steps.generate_random_number_strings(6)))
+    # отправляем запрос
+    response_get = requests_steps.request_get(urls.url_pet_get_id(support_steps.generate_random_number_strings(6)))
     print("result =", response_get.json ())
-    assert response_get.json () ['message'] == "Pet not found"
+    # убеждаемся, что питомец не найден
+    assert_steps.assert_equals_response_values(response_get.json () ['message'], "Pet not found")
 
-#получить питомца по статусу
+# проверка, получения питомца по статусу
 @pytest.mark.smoke_regression
-def test_get_by_status():
-    #создаем питомца с нужным статусом  - sold
-    request = create_request_json.generate_json_pet("dogs")
-    response_post = requests.post(urls.url_pet_post, json=request)
-    print("result=", response_post.json())
-    #выбираем питомцев
-    response = requests.get(urls.url_pet_get_findbystatus("sold"))
-    print("result_get=", response.json())
-    #выполняем проверки
-    assert response.status_code==200
-    assert response.json()[0]['status'] == "sold"
-    assert str(response.json()).__contains__(response_post.json()['name'])
+@pytest.mark.full_regression
+@pytest.mark.parametrize('status',
+                         [
+                             "sold",
+                             "available"
+                         ],
+                         ids=["sold", "available"]
+                         )
 
+def test_get_by_status(status):
+    # создаем питомца с нужным статусом (параметр)
+    print(status)
+    request = create_request_json.generate_json_pet_status(status)
+    # отправляем запрос
+    response_post = requests_steps.request_post(urls.url_pet_post, request)
+    print("result=", response_post.json())
+    # отправляем запрос на получение питомцев со статусом sold
+    response = requests_steps.request_get(urls.url_pet_get_findbystatus(status))
+    print("result_get=", response.json())
+    # выполняем проверки полученного результата
+    assert_steps.assert_response_has_status (response, 200)
+    assert_steps.assert_equals_response_values(response.json()[0]['status'],status)
+    assert_steps.assert_response_contains_text(response.json(),(response_post.json()['name']))
+
+
+# проверка поиска питомцев по заведомо не существующему статусу
 @pytest.mark.smoke_regression
 def test_get_negative_by_status():
-    #ищем питомцев по заведомо не существующему статусу
-    response = requests.get(urls.url_pet_get_findbystatus(support_steps.generate_random_letter_strings(6)))
+    # отправляем запрос
+    response = requests_steps.request_get(urls.url_pet_get_findbystatus(support_steps.generate_random_letter_strings(6)))
     print("result_get=", response.json())
-    assert response.status_code == 200
-    assert str(response.json()).__contains__("[]")
+    # проверяем, что ответ содержит статус 200 и список питомцев пуст
+    assert_steps.assert_response_has_status (response, 200)
+    assert_steps.assert_response_contains_text(response.json(), "[]")
 
 #***** Запросы DEL *****
-@pytest.mark.smoke_regression
-def test_del():
-    #проверка удаления питомца
-    #сначала создаем питомца
-    request = create_request_json.generate_json_pet("dogs")
-    response_post = requests.post(urls.url_pet_post, json=request)
-    print("result =", response_post.json())
-    #теперь удаляем питомца
-    response_del = requests.delete(urls.url_pet_get_id(str(response_post.json()["id"])))
-    print("result delete =", response_del.json())
-    assert response_del.json()['code'] == 200
-    #пробуем получить удаленного питомца
-    response_get = requests.get(urls.url_pet_get_id(str(response_post.json()["id"])))
-    assert response_get.json()['message'] == 'Pet not found'
 
+# проверка удаления питомца
+@pytest.mark.smoke_regression
+@pytest.mark.full_regression
+def test_del():
+    # создаем json для питомца
+    request = create_request_json.generate_json_pet()
+    # отправяем запрос на создание питомца
+    response_post = requests_steps.request_post(urls.url_pet_post, request)
+    print("result =", response_post.json())
+    # теперь удаляем питомца, отправляем запрос
+    response_del = requests_steps.request_del(urls.url_pet_get_id(str(response_post.json()["id"])))
+    print("result delete =", response_del.json())
+    # проверяем, что ответ содержит статус 200
+    assert_steps.assert_response_has_status (response_del, 200)
+    # пробуем получить удаленного питомца
+    response_get = requests_steps.request_get(urls.url_pet_get_id(str(response_post.json()["id"])))
+    assert_steps.assert_equals_response_values(response_get.json()['message'] , 'Pet not found')
+
+# проверка удаления несуществующего питомца
 @pytest.mark.full_regression
 def test_del_negative_id():
-    #проверка удаления несуществующего питомца
-    response_del = requests.delete(urls.url_pet_get_id(support_steps.generate_random_number_strings(6)))
+    #отправляем запрос на удаление несуществующего питомца
+    response_del = requests_steps.request_del(urls.url_pet_get_id(support_steps.generate_random_number_strings(6)))
     print("result delete =", response_del)
-    assert str(response_del).__contains__("404")
+    # проверяем, что ответ содержит статус 404
+    assert_steps.assert_response_has_status(response_del, 404)
 
 
 # pytest test_get.py::test_get -v -s
+# pytest -m smoke_regression
--- test_user_api.py
+++ test_user_api.py
@@ -4,114 +4,157 @@ import json
 import resources.urls as urls
 import Steps.support_steps as support_steps
 import Steps.create_request_json as create_request_json
+import Steps.request_steps as requests_steps
 
 #************ Тестирование API для управления пользователями **************
 
 #***** Запросы POST *****
 
-#создать пользователя
+#проверка создания пользователя с переданным username пользователя
 @pytest.mark.smoke_regression
+@pytest.mark.full_regression
+@pytest.mark.parametrize('username',
+                         [
+                             "ivan",
+                             "makar-213"
+                         ],
+                         ids=["sold", "available"]
+                         )
 def test_post_user_username(username):
-    #создать пользователя с определенным username
+    # создать json для пользователя с определенным username
     request = create_request_json.generate_json_user_username(username)
-    response_post_user = requests.post(urls.url_pet_user, json=request)
+    # отправить запрос
+    response_post_user = requests_steps.request_post(urls.url_pet_user, request)
     print("response_post_user =", response_post_user.json())
-    assert response_post_user.status_code ==200
+    # проверить, что ответ содержит статус 200
+    assert_steps.assert_response_has_status(response_post_user, 200)
 
+# проверка создания пользователя со случайным username
 @pytest.mark.smoke_regression
 def test_post_user():
-    #создать пользователя со случайным username
+    # создать json для пользователя со случайным username
     request = create_request_json.generate_json_user_random()
-    response_post_user = requests.post(urls.url_pet_user, json=request)
+    # отправить запрос
+    response_post_user = requests_steps.request_post(urls.url_pet_user, request)
     print("response_post_user =", response_post_user.json())
-    assert response_post_user.status_code ==200
+    # проверить, что ответ содержит статус 200
+    assert_steps.assert_response_has_status(response_post_user, 200)
 
 #***** Запросы GET *****
 
-#получить пользователя по юзеренейму
+# проверка получения пользователя по юзеренейму
 @pytest.mark.smoke_regression
+@pytest.mark.full_regression
 def test_get_user():
-    # подготавливаем пользователя
+    # подготавливаем юзеренейм пользователя
     username = support_steps.generate_random_letter_strings(6)
+    # создать json для пользователя с определенным username
     test_post_user_username(username)
     # подготавливаем данные для запроса get
     url_get_user = urls.url_pet_user + "/" + username
-    response_user = requests.get(url_get_user)
-    assert response_user.status_code ==200
-    assert response_user.json()["username"] == username
+    # отправить запрос
+    response_user = requests_steps.request_get(url_get_user)
+    # проверить, что ответ содержит статус 200
+    assert_steps.assert_response_has_status(response_user, 200)
+    # проверить, что ответ содержит заданный нами username
+    assert_steps.assert_equals_response_values(response_user.json()["username"], username)
 
+
+# проверка получения пользователя по несуществующему username
 @pytest.mark.full_regression
 def test_get_negative_user():
-    #пытаемся получить пользователя по несуществующему username
+    # создаем username для запроса
     username = support_steps.generate_random_letter_strings(6)
     url_get_user1 =  urls.url_pet_user + "/" + username
-    print(url_get_user1)
-    response_user1 = requests.get(url_get_user1)
-    print(response_user1)
-    assert response_user1.status_code == 404
+    # пытаемся получить пользователя по несуществующему username
+    response_user1 = requests_steps.request_get(url_get_user1)
+    # проверить, что ответ содержит 404
+    assert_steps.assert_response_has_status(response_user1, 404)
 
 
 #***** Запросы PUT *****
+
+# провека, что можно обновить данные пользователя метдом put
 @pytest.mark.smoke_regression
+@pytest.mark.full_regression
 def test_put_user():
-    #создаем пользователя
+    # создаем username для запроса
     username = support_steps.generate_random_letter_strings(6)
+    # создать json для пользователя с определенным username
     request = create_request_json.generate_json_user_username(username)
-    response_post_user = requests.post(urls.url_pet_user, json=request)
+    # отправляем запрос
+    response_post_user = requests_steps.request_post(urls.url_pet_user, request)
     print("response_post_user =", response_post_user.json())
 
     # готовим данные для обновления пользователя
     username1 = support_steps.generate_random_letter_strings(6)
+    # создать json для пользователя с новым username
     request_put = create_request_json.generate_json_user_username(username1)
+    # формируем url и отправляем запрос
     url_put_user = urls.url_pet_user + "/" + username
-    response_put_user = requests.put(url_put_user, json=request_put)
+    response_put_user = requests_steps.request_put(url_put_user, request_put)
+    # проверим, что ответ содержит 200
     print("response_put_user =", response_put_user.status_code)
-    assert response_put_user.status_code==200
-
-    #проверим, что по обновленому имени пользователя можно получить get
+    assert_steps.assert_response_has_status(response_put_user, 200)
+    # проверим, что по обновленому имени пользователя можно получить данные методом get
     url_get_user = urls.url_pet_user + "/" + username1
-    response_get_user = requests.get(url_get_user)
+    response_get_user = requests_steps.request_get(url_get_user)
     print("response_get_user =", response_get_user.status_code)
-    assert response_get_user.status_code==200
+    # проверим, что ответ содержит 200
+    assert_steps.assert_response_has_status(response_get_user, 200)
 
+# проверка, что нельзя обновить данные несуществующего пользователя метдом put
 @pytest.mark.full_regression
 def test_put_negative_user():
+    # создаем username для json запроса и для url (будем использовать разные username)
     username = support_steps.generate_random_letter_strings(6)
+    username1 = support_steps.generate_random_letter_strings(6)
+    # создать json для пользователя с данным username
     request_put = create_request_json.generate_json_user_username(username)
-    #обращаемся с запросом по имени пользовател, которого не создавали:
-    url_put_user = urls.url_pet_user + "/" + username
-    response_put_user = requests.put(url_put_user, json=request_put)
+    # обращаемся с запросом по имени пользователя, которого не создавали:
+    url_put_user = urls.url_pet_user + "/" + username1
+    response_put_user = requests_steps.request_put(url_put_user, request_put)
     print("response_put_user =", response_put_user.status_code)
-    assert response_put_user.status_code == 404
+    # проверим, что ответ содержит 404
+    assert_steps.assert_response_has_status(response_put_user, 404)
 
 
 #***** Запросы DEL *****
+
+# проверка удаления пользователя
 @pytest.mark.smoke_regression
+@pytest.mark.full_regression
 def test_del_user():
     username = support_steps.generate_random_letter_strings(6)
     #создаем пользователя
     test_post_user_username(username)
+    # формируем url для запроса
     url_del_user = urls.url_pet_user + "/" + username
     print(url_del_user)
-    #удаляем пользователя
-    response_user_del = requests.delete(url_del_user)
+    # выполняем запрос и удаляем пользователя
+    response_user_del = requests_steps.request_del(url_del_user)
     print(response_user_del)
-    assert response_user_del.status_code == 200
-    #проверим далее, что удаленного пользователя теперь не получить запросом get
-    response_user_get = requests.get(url_del_user)
-    assert response_user_get.status_code == 404
+    # проверим, что ответ содержит 200
+    assert_steps.assert_response_has_status(response_user_del, 200)
+    # проверим далее, что удаленного пользователя теперь не получить запросом get
+    response_user_get = requests_steps.request_get(url_del_user)
+    assert_steps.assert_response_has_status(response_user_get, 404)
 
-
+# проверка, что нельзя удалить пользователя, которого нет
 @pytest.mark.full_regression
 def test_del_negative_user():
-    #пробуем удалить пользователя, которого нет
+    # формируем произвольный username
     username = support_steps.generate_random_letter_strings(6)
+    # формируем url запроса
     url_del_user = urls.url_pet_user + "/" + username
-    response_user_del = requests.delete(url_del_user)
+    # отправляем запрос
+    response_user_del = requests_steps.request_del(url_del_user)
     print(response_user_del)
-    assert response_user_del.status_code == 404
+    # проверим, что ответ содержит 404
+    assert_steps.assert_response_has_status(response_user_del, 404)
 
 
 
 
 #pytest test_user_api.py::test_del_negative_user - v - s
+#pytest -m smoke_regression
